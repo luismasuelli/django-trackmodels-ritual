353ada0d47c88173f96cb84ec9f09b29567e0246
luismasuelli <luismasuelli@hotmail.com>
2016-05-21 10:44:01 -0500

    sample admin

--- sample/admin.py
+++ sample/admin.py
@@ -1,9 +1,11 @@
 from django.contrib.admin import site, ModelAdmin
-from grimoire.django.tracked.admin import CreatePeriodAgoAndCurrentFilter, UpdatePeriodAgoAndCurrentFilter
+from grimoire.django.tracked.admin import (
+    CreatePeriodAgoAndCurrentFilter, UpdatePeriodAgoAndCurrentFilter, TrackedLiveReportingMixin
+)
 from .models import SampleRecord
 
 
-class SampleAdmin(ModelAdmin):
+class SampleAdmin(TrackedLiveReportingMixin, ModelAdmin):
 
     list_filter = (CreatePeriodAgoAndCurrentFilter, UpdatePeriodAgoAndCurrentFilter)
     list_display = ('id', 'content', 'created_on', 'updated_on')
