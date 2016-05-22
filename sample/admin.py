ecab7e898465548919c0f8ae8ad5299fd0a6da09
luismasuelli <luismasuelli@hotmail.com>
2016-05-22 11:45:35 -0500

    better list report item checking and title fetching also moved reports to different file

--- sample/admin.py
+++ sample/admin.py
@@ -1,12 +1,18 @@
 from django.contrib.admin import site, ModelAdmin
 from django.utils.translation import ugettext_lazy as _
-from grimoire.django.tracked.admin import (TrackedLiveAdmin, CSVReport)
-from .models import SampleRecord
+from grimoire.django.tracked.admin import TrackedLiveAdmin
+from grimoire.django.tracked.reports import CSVReport
+from .models import SampleRecord, uppercase_content
+
+
+class CustomCSVReport(CSVReport):
+
+    list_report = ('id', 'content', 'created_on', 'updated_on', uppercase_content)
 
 
 class SampleAdmin(TrackedLiveAdmin, ModelAdmin):
 
-    report_generators = [CSVReport('csv', _('CSV Report'))]
+    report_generators = [CustomCSVReport('csv', _('CSV Report'))]
     list_display = ('id', 'content', 'created_on', 'updated_on')
 
 
--- sample/models.py
+++ sample/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from grimoire.django.tracked import models as tracked_models
 
 
+def uppercase_content(obj):
+    return (obj.content or '').upper()
+uppercase_content.short_description = 'content to uppercase'
+
+
 class SampleRecord(tracked_models.TrackedLiveAndDead):
     """
     A dummy sample record, ready to be included in Admin.
