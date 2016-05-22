54d24da4cf6351cb0141e6cbc3cf1d70ce1c0f5f
luismasuelli <luismasuelli@hotmail.com>
2016-05-21 20:59:58 -0500

    csv reports and features like that

--- grimoire/django/tracked/admin.py
+++ grimoire/django/tracked/admin.py
@@ -4,9 +4,17 @@ from django.contrib.admin import SimpleListFilter, ModelAdmin
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
 from django.utils.encoding import force_text
+from django.utils.timezone import now
 from django.utils.translation import ugettext_lazy as _
 from abc import ABCMeta, abstractmethod
+from collections import namedtuple
+from cantrips import functions
+from django.utils.six import text_type
+import StringIO
+import datetime
 import logging
+import csv
+import io
 
 
 logger = logging.getLogger(__name__)
@@ -83,6 +91,10 @@ class PeriodFilter(SimpleListFilter):
             return queryset
 
 
+TrackingReportColumn = namedtuple('TrackingReportColumn', ('header', 'fetcher'))
+TrackingReportResult = namedtuple('TrackingReportResult', ('headers', 'values'))
+
+
 class TrackingReport(object):
     """
     This is an abstract class to process a report for a given format, model, and period.
@@ -93,7 +105,14 @@ class TrackingReport(object):
        the `content_type` member (a string).
     3. The attachment name. This MUST be defined by overriding `get_attachment_filename` method.
     4. The attachment content. This MUST be defined by overriding `get_attachment_content` method.
+    5. The fields to report. This can be defined by overriding `get_list_report` method or just
+       the `list_report` member (a list of strings).
     """
+
+    list_report = []
+    datetime_format = "%Y-%m-%d %H:%M:%S"
+    date_format = "%Y-%m-%d"
+    time_format = "%H:%M:%S"
 
     __metaclass__ = ABCMeta
     content_type = None
@@ -107,6 +126,20 @@ class TrackingReport(object):
 
         self._key = key
         self._text = text
+
+    def _serialize_value(self, value):
+        """
+        Serialize values like date and time. Respect other values:
+        :return:
+        """
+
+        if isinstance(value, datetime.datetime):
+            return value.strftime(self.datetime_format)
+        elif isinstance(value, datetime.date):
+            return value.strftime(self.date_format)
+        elif isinstance(value, datetime.time):
+            return value.strftime(self.time_format)
+        return text_type(value)
 
     @property
     def key(self):
@@ -124,26 +157,145 @@ class TrackingReport(object):
 
         return self.content_type
 
-    @abstractmethod
-    def get_attachment_filename(self):
+    def get_list_report(self, request):
         """
-        Filename to be used for the attachment.
+        Fields this report will handle.
+        :param request: If the overriden logic will depend on the request.
+        :return: A list of strings with fields to report.
+        """
+
+        return self.list_report
+
+    def get_report_column_builders(self, request, model):
+        """
+        Returns builders for column names and column values
+          for the elements, being each element like this:
+          1. A header fetcher: will retrieve the title for the column.
+          2. A value fetcher: will retrieve the value for the column.
+        :param model: model to analyze and fetch.
+        :param request: current request being processed.
+        :return: A list of TrackingReportColumn pairs.
+        """
+
+        meta = model._meta
+        field_names = set(field.name for field in meta.fields)
+        list_report = self.get_list_report(request) or field_names
+        _s = self._serialize_value
+
+        def header(list_report_item):
+            if list_report_item in field_names:
+                return text_type(meta.get_field(list_report_item).verbose_name or list_report_item)
+            else:
+                # model member (method or property)
+                model_member = getattr(model, list_report_item, None)
+                # method check
+                if functions.is_method(model_member, functions.METHOD_UNBOUND|functions.METHOD_INSTANCE):
+                    return text_type(getattr(model_member, 'short_description', list_report_item))
+                # property check
+                if isinstance(model_member, property):
+                    if model_member.getter:
+                        return text_type(getattr(model_member, 'short_description', list_report_item))
+                    raise ValueError('Property item in `list_report` member, or returned by `get_list_report()` '
+                                     'must be readable')
+
+                # report member (method)
+                report_member = getattr(self, list_report_item, None)
+                if functions.is_method(report_member, functions.METHOD_UNBOUND|functions.METHOD_INSTANCE):
+                    return text_type(getattr(report_member, 'short_description', list_report_item))
+
+                # regular callable
+                if callable(list_report_item):
+                    return text_type(getattr(list_report, 'short_description', None) or
+                                     getattr(list_report, '__name__', None) or '<unknown>')
+
+                # invalid value
+                raise TypeError('Item in `list_report` member, or returned by `get_list_report()` must be a model '
+                                'field name, or model instance method, or current report''s instance method')
+
+        def fetcher(list_report_item):
+            if list_report_item in field_names:
+                return lambda obj: _s(getattr(obj, list_report_item))
+            else:
+                # model member (method or property)
+                model_member = getattr(model, list_report_item, None)
+                # method check
+                if functions.is_method(model_member, functions.METHOD_UNBOUND|functions.METHOD_INSTANCE):
+                    return lambda obj: _s(getattr(obj, list_report_item)())
+                # property check
+                if isinstance(model_member, property):
+                    if model_member.getter:
+                        return lambda obj: _s(getattr(obj, list_report_item))
+                    raise ValueError('Property item in `list_report` member, or returned by `get_list_report()` '
+                                     'must be readable')
+
+                # report member (method)
+                report_member = getattr(self, list_report_item, None)
+                if functions.is_method(report_member, functions.METHOD_UNBOUND|functions.METHOD_INSTANCE):
+                    return lambda obj: _s(getattr(self, list_report_item)(obj))
+
+                # regular callable
+                if callable(list_report_item):
+                    return lambda obj: _s(list_report_item(obj))
+
+                # invalid value
+                raise TypeError('Item in `list_report` member, or returned by `get_list_report()` must be a model '
+                                'field name, or model instance method, or current report''s instance method')
+
+        return [TrackingReportColumn(header=header(item), fetcher=fetcher(item)) for item in list_report]
+
+    def get_report_data_rows(self, request, queryset):
+        """
+        Using the builders for the queryset model, iterates over the queryset to generate a result
+          with headers and rows. This queryset must be the exact same received in the .process method,
+          which tells us that this function should be called inside .process implementation.
+        :param queryset: Provided queryset
+        :return: Result with headers and rows
+        """
+
+        model = queryset.model
+        meta = model._meta
+        field_names = set(field.name for field in meta.fields)
+        list_report = self.get_list_report(request) or field_names
+        queried_field_named = [l for l in list_report if l in field_names] or ['id']
+
+        columns = self.get_report_column_builders(request, model)
+        headers = [column.header for column in columns]
+        rows = []
+        for instance in queryset.only(*queried_field_named):
+            rows.append([column.fetcher(instance) for column in columns])
+
+        return TrackingReportResult(headers=headers, values=rows)
+
+    @abstractmethod
+    def get_attachment_filename(self, request, period):
+        """
+        Filename to be used for the attachment
+        :param request: Requset being processed.
+        :param period: Period being queried.
         :return: A string with the filename.
         """
 
         return 'override.me'
 
     @abstractmethod
-    def get_attachment_content(self, request, model, period):
+    def dump_report_content(self, result):
         """
-        Returns the generated file content.
-        :param request: The request being processed.
-        :param model: The model class being processed.
-        :param period: The model being processed.
-        :return: The report content (usually expressed in raw bytes but could be unicode as well).
+        Dumps the content to a string, suitable to being written on a file.
+        :param result:
+        :return: string
         """
 
         return b''
+
+    def get_attachment_content(self, request, queryset):
+        """
+        Returns the generated file content.
+        :param request: The request being processed.
+        :param queryset: The model class being processed.
+        :return: The report content (usually expressed in raw bytes but could be unicode as well).
+        """
+
+        return self.dump_report_content(self.get_report_data_rows(request, queryset))
 
     def process(self, request, queryset, period):
         """
@@ -154,10 +306,41 @@ class TrackingReport(object):
         :return: The response with the report.
         """
 
-        response = HttpResponse(content=self.get_attachment_content(request, queryset.model, period) or '',
+        response = HttpResponse(content=self.get_attachment_content(request, queryset) or '',
                                 content_type=self.get_attachment_content_type() or 'text/plain')
-        response['Content-Disposition'] = 'attachment; filename=' + (self.get_attachment_filename() or 'report.txt')
+        response['Content-Disposition'] = 'attachment; filename=' + (self.get_attachment_filename(request, period) or
+                                                                     'report.txt')
         return response
+
+
+class CSVReport(TrackingReport):
+    """
+    CSV dumping report. No plugins required for it.
+    """
+
+    csv_kwargs = {}
+
+    def get_attachment_filename(self, request, period):
+        """
+        Filename to be used for the attachment
+        :param request: Requset being processed.
+        :param period: Period being queried.
+        :return: A string with the filename.
+        """
+
+        return 'report-%s.csv' % now().strftime("%Y%m%d%H%M%S")
+
+    def dump_report_content(self, result):
+        """
+        Dumps the content to a string, suitable to being written on a file.
+        :param result: The result being processed.
+        :return: string
+        """
+
+        output = io.StringIO()
+        writer = csv.writer(output, **self.csv_kwargs)
+        writer.writerows([result.headers] + result.values)
+        return output.getvalue()
 
 
 class TrackedLiveAdmin(ModelAdmin):
